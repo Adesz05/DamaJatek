3ce5c1a243516d170fed47578386fc07eba89f49
kkrisztian <kerteszkrisztian@turr.hu>
2023-01-11 09:33:14 +0100

    dsd

--- Dáma/Form1.cs
+++ Dáma/Form1.cs
@@ -12,9 +12,36 @@ namespace Dáma
 {
     public partial class Form1 : Form
     {
+        static int meret = 8;
+        static Mezo[,] tabla = new Mezo[meret,meret];
         public Form1()
         {
+            MatrixGeneralas();
+
             InitializeComponent();
+        }
+
+        private void MatrixGeneralas()
+        {
+            bool vilagos = true;
+            for (int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    string szin = (vilagos) ? "vilagos" : "sotet";
+                    tabla[i, j] = new Mezo(new PictureBox(), false, szin);
+                    vilagos = !vilagos;
+                    
+                }
+            }
+            for(int i = 0; i < meret; i++)
+            {
+                for (int j = 0; j < meret; j++)
+                {
+                    MessageBox.Show( tabla[i, j].MelyikSzin);
+
+                }
+            }
         }
     }
 }
---
+++ Dáma/Mezo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dáma
+{
+    class Mezo
+    {
+        public PictureBox mezo;
+        public bool VaneRajta;
+        public string MelyikSzin;
+
+        public Mezo(PictureBox mezo, bool vaneRajta, string melyikszin)
+        {
+            this.mezo = mezo;
+            VaneRajta = vaneRajta;
+            MelyikSzin = melyikszin;
+        }
+    }
+}
