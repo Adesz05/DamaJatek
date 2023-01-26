114641ba0db14058542a006ea8ac94a0d4a53e42
Adesz05 <kerekadamtamas@turr.hu>
2023-01-26 12:30:37 +0100

    ujra ütünk

--- Dáma/Form1.cs
+++ Dáma/Form1.cs
@@ -77,9 +77,9 @@ namespace Dáma
         private void Klikkeles(object sender, EventArgs e)
         {
             Mezo klikkelt = sender as Mezo;
+
             //MessageBox.Show(klikkelt.Koordinatak.X + " meg " + klikkelt.Koordinatak.Y);
             //fehér------------------------------------------------------------------------------------------------------------------------------------------------------
-            KijelolesekTorlese();
             if (klikkelt.MelyikBabu!="üres")
             {
                 if (klikkelt.MelyikBabu == kijon || klikkelt.MelyikBabu==kijon+"dáma")
@@ -88,7 +88,9 @@ namespace Dáma
                     {
                         if (uthetomezok.Count(x => x.Uto == klikkelt) > 0)
                         {
+                            KijelolesekTorlese();
                             UthetoMezokMegjelenitese(klikkelt);
+                            kijeloltbabu = klikkelt;
                         }
                     }
                     //Nincs ütéskényszer
@@ -97,122 +99,92 @@ namespace Dáma
                         //a kijelölt bábu tud-e mozogni
                         if (TudeMozogni(klikkelt))
                         {
-                        MessageBox.Show("fekete tud mozogni");
-
+                            KijelolesekTorlese();
+                            LephetoMezoKijeloles(klikkelt);
+                            kijeloltbabu = klikkelt;
                         }
                     }
                 }
             }
-
-            /*if (!vanekijeloltbabu)
-            {
-                if (klikkelt.MelyikBabu == kijon)
-                {
-                    //balra lehet mozogni
-                    if (klikkelt.Koordinatak.X != 0)
-                    {
-                        //balra 1db átló mélység vizsgálat
-                        if (tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1].MelyikBabu == "üres")
-                        {
-                            tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1].Image = Properties.Resources.sotetzoldpotyivelakozepen;
-                            mezokahovalepnilehet.Add(tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1]);
-                            vanekijeloltbabu = true;
-                            kijeloltbabu = klikkelt; 
-                        }
-                        //ha balra az ellenfél bábuja amit ütni szeretnénk nem a fal mellett áll
-                        //a sajat babut ne lehessen leutni TODO
-                        else if (klikkelt.Koordinatak.X - 1 != 0 && tabla[klikkelt.Koordinatak.X - 2, klikkelt.Koordinatak.Y - 2].MelyikBabu == "üres" && tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1].MelyikBabu != "fehér") //vagy dáma
-                        {
-                            tabla[klikkelt.Koordinatak.X - 2, klikkelt.Koordinatak.Y - 2].Image = Properties.Resources.sotetzoldpotyivelakozepen;
-                            mezokahovalepnilehet.Add(tabla[klikkelt.Koordinatak.X - 2, klikkelt.Koordinatak.Y - 2]);
-                            vanekijeloltbabu = true;
-
-                            uthetomezok.Add(tabla[klikkelt.Koordinatak.X-2,klikkelt.Koordinatak.Y-2],tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1]);
-                            kijeloltbabu = klikkelt;
-                            uteskenyszer = true;
-                        }
-                    }
-
-                    //jobbra lehet mozogni
-                    if (klikkelt.Koordinatak.X != 7)
-                    {
-                        //jobbra 1db átló mélység vizsgálat
-                        if (tabla[klikkelt.Koordinatak.X + 1, klikkelt.Koordinatak.Y - 1].MelyikBabu == "üres" && !uteskenyszer)
-                        {
-                            tabla[klikkelt.Koordinatak.X + 1, klikkelt.Koordinatak.Y - 1].Image = Properties.Resources.sotetzoldpotyivelakozepen;
-                            mezokahovalepnilehet.Add(tabla[klikkelt.Koordinatak.X + 1, klikkelt.Koordinatak.Y - 1]);
-                            vanekijeloltbabu = true;
-                            kijeloltbabu = klikkelt;
-                        }
-                        //ha balra az ellenfél bábuja amit ütni szeretnénk nem a fal mellett áll
-                        else if(klikkelt.Koordinatak.X + 1 !=7 && tabla[klikkelt.Koordinatak.X + 2, klikkelt.Koordinatak.Y - 2].MelyikBabu == "üres" && tabla[klikkelt.Koordinatak.X + 1, klikkelt.Koordinatak.Y - 1].MelyikBabu != "fehér") // dáma //fekete meg nem jo
-                        {
-                            tabla[klikkelt.Koordinatak.X + 2, klikkelt.Koordinatak.Y - 2].Image = Properties.Resources.sotetzoldpotyivelakozepen;
-                            mezokahovalepnilehet.Add(tabla[klikkelt.Koordinatak.X + 2, klikkelt.Koordinatak.Y - 2]);
-                            vanekijeloltbabu = true;
-                            uthetomezok.Add(tabla[klikkelt.Koordinatak.X + 2, klikkelt.Koordinatak.Y - 2],tabla[klikkelt.Koordinatak.X + 1, klikkelt.Koordinatak.Y - 1]);
-                            kijeloltbabu = klikkelt;
-                            uteskenyszer = true;
-                        }
-                       
-
-                    }
-                    if (uteskenyszer && tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1].MelyikBabu == "üres")
-                    {
-                        tabla[klikkelt.Koordinatak.X - 1, klikkelt.Koordinatak.Y - 1].Image = Properties.Resources.sotet;
-                    }
-                    uteskenyszer = false;
-                }
-            }
-            //van már egy kijelölt bábu
             else
             {
-                //MessageBox.Show("vankijeloltbabu");
-                vanekijeloltbabu = false;
-                for (int i = 0; i < mezokahovalepnilehet.Count; i++)
+                //üres mezőre kattint
+                if (kijeloltbabu!=null)
                 {
-                    tabla[mezokahovalepnilehet[i].Koordinatak.X, mezokahovalepnilehet[i].Koordinatak.Y].Image = Properties.Resources.sotet;
-                    if (klikkelt== mezokahovalepnilehet[i])
+                    if (klikkelt.MelyikSzin == "kijelölt")
                     {
-                        //egyenlőre csak sima amogus bábu léphet oda
-                        if (kijon=="fehér")
+                        if (uthetomezok.Count(x => x.Lepheto==klikkelt && x.Uto == kijeloltbabu) > 0)
                         {
-                            tabla[mezokahovalepnilehet[i].Koordinatak.X, mezokahovalepnilehet[i].Koordinatak.Y].Image =Properties.Resources.sotetmezoFeherAmogaval;
-                        }
-                        else
-                        {
-                            tabla[mezokahovalepnilehet[i].Koordinatak.X, mezokahovalepnilehet[i].Koordinatak.Y].Image =Properties.Resources.sotetmezoFeketeAmogaval;
-
-                        }
-                        tabla[mezokahovalepnilehet[i].Koordinatak.X, mezokahovalepnilehet[i].Koordinatak.Y].MelyikBabu=kijeloltbabu.MelyikBabu;
-                        tabla[kijeloltbabu.Koordinatak.X, kijeloltbabu.Koordinatak.Y].Image = Properties.Resources.sotet;
-                        tabla[kijeloltbabu.Koordinatak.X, kijeloltbabu.Koordinatak.Y].MelyikBabu="üres";
-                        foreach (KeyValuePair<Mezo, Mezo> item in uthetomezok)
-                        {
-                            if (item.Key.Koordinatak==klikkelt.Koordinatak)
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Lepheto.MelyikBabu = kijeloltbabu.MelyikBabu;
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Lepheto.Image = kijeloltbabu.Image;
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Lepheto.MelyikSzin="sötét";
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Utheto.MelyikBabu="üres";
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Utheto.Image = Properties.Resources.sotet;
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Uto.Image = Properties.Resources.sotet;
+                            uthetomezok.Find(x => x.Lepheto == klikkelt && x.Uto == kijeloltbabu).Uto.MelyikBabu="üres";
+                            kijeloltbabu = null;
+                            KijelolesekTorlese();
+                            uthetomezok.Clear();
+                            EgyBabuUtoKenyszer(klikkelt.Koordinatak.X, klikkelt.Koordinatak.Y);
+                            if (uthetomezok.Count > 0)
                             {
-                                item.Value.Image = Properties.Resources.sotet;
-                                item.Value.MelyikBabu= "üres";
-
+                                //ujra ütünk
                             }
+
+
                         }
+                        //Mozgunk oda
+                    }
+                    else
+                    {
+                        //levesszük a kijelölést
                     }
                 }
-                mezokahovalepnilehet.Clear();
-                uthetomezok.Clear();
             }
-            
+        }
 
-            //dáma if
-            // || klikkelt.MelyikBabu == kijon + "dáma"
-            */
+        private void LephetoMezoKijeloles(Mezo klikkelt)
+        {
+            //int irany = klikkelt.MelyikBabu.Contains("fehér") ? -1 : 1;
+            int hanyatlephet = klikkelt.MelyikBabu.Contains("dáma") ? meret - 1 : 1;
+            for (int i = -1; i <= 1; i += 2)
+            {
+                for (int j = -1; j <= 1; j += 2)
+                {
+                    for (int k = 1; k <= hanyatlephet; k++)
+                    {
+                        try
+                        {
+                            if (klikkelt.MelyikBabu.Contains("dáma") || klikkelt.MelyikBabu.Contains("fehér") && j*k < 0)
+                            {
+                                if (tabla[klikkelt.Koordinatak.X + i*k, klikkelt.Koordinatak.Y + j*k].MelyikBabu == "üres")
+                                {
+                                    tabla[klikkelt.Koordinatak.X + i *k, klikkelt.Koordinatak.Y + j*k].Image = Properties.Resources.sotetzoldpotyivelakozepen;
+                                    tabla[klikkelt.Koordinatak.X + i * k, klikkelt.Koordinatak.Y + j*k].MelyikSzin = "kijelölt";
+                                }
+                                else { break; }
+                            }
+                            if (klikkelt.MelyikBabu.Contains("dáma") || klikkelt.MelyikBabu.Contains("fekete") && j*k > 0)
+                            {
+                                if (tabla[klikkelt.Koordinatak.X + i*k, klikkelt.Koordinatak.Y + j*k].MelyikBabu == "üres")
+                                {
+                                    tabla[klikkelt.Koordinatak.X + i*k, klikkelt.Koordinatak.Y + j*k].Image = Properties.Resources.sotetzoldpotyivelakozepen;
+                                    tabla[klikkelt.Koordinatak.X + i*k, klikkelt.Koordinatak.Y + j*k].MelyikSzin = "kijelölt";
+                                }
+                                else { break; }
+                            }
+                        }
+                        catch (Exception) { break; }
+                    }
+                }
+            }
         }
 
         private bool TudeMozogni(Mezo klikkelt)
         {
-            for (int i = -1; i < 1; i+=2)
+            for (int i = -1; i <= 1; i+=2)
             {
-                for (int j = -1; j < 1; j+=2)
+                for (int j = -1; j <= 1; j+=2)
                 {
                     try
                     {
@@ -273,40 +245,44 @@ namespace Dáma
                 {
                     if (tabla[i,j].MelyikBabu.Contains(kijon))
                     {
-                        for (int irany1 = -1; irany1 <= 1; irany1+=2)
-                        {
-                            for (int irany2 = -1; irany2 <= 1; irany2+=2)
-                            {
-                                for (int k = 1; k <= (tabla[i, j].MelyikBabu.Contains("dáma") ? 8 : 1); k++)
-                                {
-                                    try
-                                    {
-                                        if (tabla[i + irany1 * k, j + irany2 * k].MelyikBabu.Contains("üres"))
-                                        {
-                                            continue;
-                                        }
-                                        if ( !tabla[i + irany1 * k, j + irany2 * k].MelyikBabu.Contains(kijon))
-                                        {
-                                            if (tabla[i + irany1 * (k+1), j + irany2 * (k+1)].MelyikBabu.Contains("üres"))
-                                            {
-                                                UthetoMezoMentese(i, j, i+irany1*k, j+irany2*k, i + irany1 * (k+1), j + irany2 * (k+1));
-                                            }
-                                            break;
-                                        }
-                                        else
-                                        {
-                                            break;
-                                        }
-                                    }
-                                    catch (Exception) { }
-                                }
-                            }
-                        }
-                        
+                        EgyBabuUtoKenyszer(i, j);
                     }
                 }
             }
             return uthetomezok.Count != 0;
+        }
+
+        private void EgyBabuUtoKenyszer(int i, int j)
+        {
+            for (int irany1 = -1; irany1 <= 1; irany1 += 2)
+            {
+                for (int irany2 = -1; irany2 <= 1; irany2 += 2)
+                {
+                    for (int k = 1; k <= (tabla[i, j].MelyikBabu.Contains("dáma") ? meret - 1 : 1); k++)
+                    {
+                        try
+                        {
+                            if (tabla[i + irany1 * k, j + irany2 * k].MelyikBabu.Contains("üres"))
+                            {
+                                continue;
+                            }
+                            if (!tabla[i + irany1 * k, j + irany2 * k].MelyikBabu.Contains(kijon))
+                            {
+                                if (tabla[i + irany1 * (k + 1), j + irany2 * (k + 1)].MelyikBabu.Contains("üres"))
+                                {
+                                    UthetoMezoMentese(i, j, i + irany1 * k, j + irany2 * k, i + irany1 * (k + 1), j + irany2 * (k + 1));
+                                }
+                                break;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        catch (Exception) { break; }
+                    }
+                }
+            }
         }
 
         private void UthetoMezoMentese(int utoX, int utoY, int uthetoX, int uthetoY, int lephetoX, int lephetoY)
@@ -316,11 +292,6 @@ namespace Dáma
 
         }
 
-        private void BalOldaliKijelolesVisszaAllitasa()
-        {
-            
-        }
-
         private void MatrixGeneralas()
         {
             int futasokszama = 0;
@@ -328,7 +299,7 @@ namespace Dáma
             bool vilagos = true;
             string melyikbabu = "üres";
 
-            /*
+            
             for (int i = 0; i < meret; i++)
             {
                 for (int j = 0; j < meret; j++)
@@ -339,7 +310,7 @@ namespace Dáma
                         feherbabuk++;
                     }
                     else melyikbabu = "üres";
-                    if (futasokszama > 39 && !vilagos)
+                    if (futasokszama > 39 && !vilagos && futasokszama%2==0)
                     {
                         melyikbabu = "fehérdáma";
                     }
@@ -348,10 +319,10 @@ namespace Dáma
                     if (j!=7) vilagos = !vilagos;
                     futasokszama++;
                 }
-            }*/
+            }
 
             //ütéses mozagasos
-            
+            /*
              for (int i = 0; i < meret; i++)
             {
                 for (int j = 0; j < meret; j++)
@@ -372,7 +343,7 @@ namespace Dáma
                     futasokszama++;
                 }
             }
-            
+            */
              
         }
     }
